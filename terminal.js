75c1af26dea612e3ad6e67cb9cf4162a15832491
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-02-10 00:07:23 -0500

    terminal functionality

---
+++ terminal.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const terminal = document.querySelector('form');
+  terminal.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const code = document.querySelector('#terminal').value;
+    document.querySelector('#terminal').value = '';
+    document.querySelector('#console').value = Function(`"use strict"; return (` + code + `)`)();
+  });
+});
