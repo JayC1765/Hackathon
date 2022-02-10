6bad7322a88960b26448df17891063053f86ec77
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-02-09 22:44:59 -0500

    debugged

--- sentance.js
+++ sentance.js
@@ -1,5 +1,7 @@
 const listOfImages = document.querySelectorAll('img');
 
+const url = chrome.runtime.getURL('thumb@2x.jpg');
+
 for (let i = 0; i < listOfImages.length; i += 1) {
-  listOfImages[i].setAttribute('src', './Images/thumb@2x.jpg');
-};
+  listOfImages[i].setAttribute('src', url);
+}
